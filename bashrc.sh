402ae2cd823e4e66a086d2d9b834c14249ea91b9
Vasily Gerasimov <UgnineSirdis@ydb.tech>
2024-08-20 08:05:58 +0000

    Fix SSH_AUTH_SOCK for tmux

--- bashrc.sh
+++ bashrc.sh
@@ -30,3 +30,10 @@ alias yab='$($SCRIPTS_DIR/repository_root.py)/ya make --build relwithdebinfo'
 
 # build and test
 alias yat='$($SCRIPTS_DIR/repository_root.py)/ya make -t -A --build relwithdebinfo'
+
+# Fix SSH auth socket location so agent forwarding works with tmux
+if test "$SSH_AUTH_SOCK" ; then
+    ln -sf $SSH_AUTH_SOCK ~/.ssh/ssh_auth_sock
+fi
+
+SSH_AUTH_SOCK=~/.ssh/ssh_auth_sock
