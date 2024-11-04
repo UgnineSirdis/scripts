e2bbaceebc1204294ac53acfc3556c1a6b7703aa
Vasily Gerasimov <UgnineSirdis@ydb.tech>
2024-11-04 06:26:24 +0000

    Server name. Remove screen aliases. Installation improvements

--- bashrc.sh
+++ bashrc.sh
@@ -1,8 +1,30 @@
 #! /usr/bin/env bash
 
-_BLUE='\[\033[01;34m\]'
+_BLACK='\[\033[0;30m\]'
+_DARK_GREY='\[\033[1;30m\]'
+_RED='\[\033[0;31m\]'
+_LIGHT_RED='\[\033[1;31m\]'
+_GREED='\[\033[0;32m\]'
+_LIGHT_GREEN='\[\033[1;32m\]'
+_ORANGE='\[\033[0;33m\]'
+_YELLOW='\[\033[1;33m\]'
+_BLUE='\[\033[0;34m\]'
+_LIGHT_BLUE='\[\033[01;34m\]'
+_PURPLE='\[\033[0;35m\]'
+_LIGHT_PURPLE='\[\033[1;35m\]'
+_CIAN='\[\033[0;36m\]'
+_LIGHT_CIAN='\[\033[1;36m\]'
+_LIGHT_GREY='\[\033[0;37m\]'
+_WHITE='\[\033[01;37m\]'
 _USUAL='\[\033[00m\]'
-export PS1="$_BLUE\w$_USUAL\$ "
+
+SERVER_NAME=""
+if test -f "$HOME/.servername" ; then
+    SERVER_NAME=$(cat "$HOME/.servername")
+    SERVER_NAME="$_WHITE$SERVER_NAME$_USUAL:"
+fi
+
+export PS1="$SERVER_NAME$_LIGHT_BLUE\w$_USUAL\$ "
 
 export TMPDIR="$HOME/tmp"
 
@@ -10,16 +32,6 @@ ulimit -c unlimited
 
 alias ll='ls -alF --color=auto'
 alias ..='cd ..'
-
-alias scr='screen -d -R -S main1'
-alias scr1='screen -d -R -S main1'
-alias scr2='screen -d -R -S main2'
-alias scr3='screen -d -R -S main3'
-alias scr4='screen -d -R -S main4'
-alias scr5='screen -d -R -S main5'
-alias scr6='screen -d -R -S main6'
-
-alias lscr='screen -list'
 
 alias cdr='cd $($SCRIPTS_DIR/repository_root.py)'
 
@@ -33,6 +45,7 @@ alias yat='$($SCRIPTS_DIR/repository_root.py)/ya make -t -A --build relwithdebin
 
 # tmux
 alias ta='tmux attach -t'
+alias tls='tmux ls'
 
 # Fix SSH auth socket location so agent forwarding works with tmux
 if test "$SSH_AUTH_SOCK" ; then
--- install_scripts.py
+++ install_scripts.py
@@ -1,6 +1,7 @@
 #! /usr/bin/env python3
 
 import os
+import shutil
 
 def get_scripts_dir() -> str:
     return os.path.dirname(os.path.abspath(__file__))
@@ -24,6 +25,19 @@ def add_to_the_end(p: str, s: str):
         f.write("\n")
 
 
+def install_config_to_home_dir(cfg_name):
+    print(f"Copy {cfg_name}")
+    shutil.copyfile(os.path.join(get_scripts_dir(), cfg_name), os.path.expanduser(f"~/{cfg_name}"))
+
+
+def install_server_name():
+    name = input("(Optional) Short server name for command prompt:\n")
+    if name:
+        name = name.strip()
+        with open(os.path.expanduser("~/.servername"), "w") as n:
+            n.write(name)
+
+
 def install():
     if not file_has_scripts_dir("~/.bashrc"):
         print("Install bashrc...")
@@ -35,5 +49,9 @@ def install():
         scripts_dir = get_scripts_dir()
         profile_path = os.path.join(scripts_dir, "profile.sh")
         add_to_the_end("~/.profile", f"export SCRIPTS_DIR=\"{scripts_dir}\"\nsource {profile_path}")
+
+    install_config_to_home_dir(".tmux.conf")
+    install_config_to_home_dir(".gitconfig")
+    install_server_name()
 
 install()
