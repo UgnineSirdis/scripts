63a19dd25d859857fdd9a6d06d0c99d2f9f0d274
Vasily Gerasimov <UgnineSirdis@ydb.tech>
2024-12-08 09:10:59 +0000

    Fixes in ydb/clone_repo.sh

--- ydb/clone_repo.sh
+++ ydb/clone_repo.sh
@@ -3,7 +3,8 @@ set -e
 
 cd $HOME
 git clone git@github.com:UgnineSirdis/ydb.git
+cd $HOME/ydb
 git branch -m main origin-main
+git remote add -m main -t main upstream git@github.com:ydb-platform/ydb.git
 git fetch upstream main
-git checkout upstream/main
-git branch -m upstream/main main
+git checkout -b main upstream/main
