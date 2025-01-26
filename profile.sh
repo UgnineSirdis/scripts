2bed5dbc1fff996b9d726d9f890927239970e990
Vasily Gerasimov <UgnineSirdis@ydb.tech>
2025-01-26 12:09:21 +0100

    bin path

--- profile.sh
+++ profile.sh
@@ -1,6 +1,6 @@
 #! /usr/bin/env bash
 
-PATH="$PATH:$SCRIPTS_DIR:/usr/libexec/docker/cli-plugins:$HOME/go/bin"
+PATH="$PATH:$SCRIPTS_DIR:/usr/libexec/docker/cli-plugins:$HOME/go/bin:$HOME/bin"
 export TMPDIR="$HOME/tmp"
 
 cd ~/ydb
