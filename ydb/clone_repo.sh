04ce59ecad548e250a325890c0e85be7e9ddd2ea
Vasily Gerasimov <UgnineSirdis@ydb.tech>
2025-01-26 10:53:05 +0100

    Fix clone script to track all branches

--- ydb/clone_repo.sh
+++ ydb/clone_repo.sh
@@ -5,6 +5,6 @@ cd $HOME
 git clone git@github.com:UgnineSirdis/ydb.git
 cd $HOME/ydb
 git branch -m main origin-main
-git remote add -m main -t main upstream git@github.com:ydb-platform/ydb.git
+git remote add -m main upstream git@github.com:ydb-platform/ydb.git
 git fetch upstream main
 git checkout -b main upstream/main
