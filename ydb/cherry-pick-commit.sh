665ca74da46e5d5b924b3fbc76edbfe77146a3af
Vasily Gerasimov <UgnineSirdis@ydb.tech>
2025-01-27 16:36:02 +0100

    cherry-pick-commit.sh

---
+++ ydb/cherry-pick-commit.sh
@@ -0,0 +1,14 @@
+#! /usr/bin/env bash
+set -e
+
+# $1 branch to merge
+# $2 commit to merge
+BRANCH=$1
+COMMIT=$2
+MERGE_BRANCH=$BRANCH-merge-$COMMIT
+
+git switch $BRANCH
+git pull
+git checkout -b $MERGE_BRANCH $BRANCH
+git cherry-pick -x $COMMIT
+git push --set-upstream origin $MERGE_BRANCH
