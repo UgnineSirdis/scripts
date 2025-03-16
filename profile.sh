6d08e7dac7c28aee4bb96e7c4946a5cc4322bcf4
Vasily Gerasimov <UgnineSirdis@ydb.tech>
2025-03-16 16:19:35 +0100

    PATH with more home priority

--- profile.sh
+++ profile.sh
@@ -1,6 +1,6 @@
 #! /usr/bin/env bash
 
-PATH="$PATH:$SCRIPTS_DIR:/usr/libexec/docker/cli-plugins:$HOME/bin:$HOME/bin/go/bin"
+PATH="$SCRIPTS_DIR:/usr/libexec/docker/cli-plugins:$HOME/bin:$HOME/bin/go/bin:$PATH"
 export TMPDIR="$HOME/tmp"
 
 cd ~/ydb
