a6600cb1d05d2618cf1326b039279903dc67f832
Vasily Gerasimov <UgnineSirdis@ydb.tech>
2025-05-28 11:54:44 +0300

    dynconfig scripts

--- ydb/slice_config/fetch_dynconfig.sh
+++ ydb/slice_config/fetch_dynconfig.sh
@@ -7,8 +7,11 @@ if [ "$1" == "" ] ; then
     exit 1
 fi
 
+SLICE_KUBECTL_CONTEXT=bastion-man-man-ydb-dev
+JUMP_HOST_NAMESPACE=ydb-dev
+
 YDB_TOKEN=$(npc --profile newbius iam get-access-token)
-kubectl config use-context ik8s-beta
+kubectl config use-context $SLICE_KUBECTL_CONTEXT
 
 # Call ydb admin config fetch
-kubectl -n ydb-global exec -it ydb-jump-host-0 -- env YDB_TOKEN=$YDB_TOKEN ydb -e grpcs://man0-0505.ydb-dev.ik8s.nebiuscloud.net:2135 -d /ydb-slice-1 admin config fetch > $1
+kubectl -n $JUMP_HOST_NAMESPACE exec -it ydb-jump-host-0 -- env YDB_TOKEN=$YDB_TOKEN ydb -e grpcs://man0-0505.ydb-dev.ik8s.nebiuscloud.net:2135 -d /ydb-slice-1 admin config fetch > $1
--- ydb/slice_config/replace_dynconfig.sh
+++ ydb/slice_config/replace_dynconfig.sh
@@ -7,11 +7,14 @@ if [ "$1" == "" ] ; then
     exit 1
 fi
 
+SLICE_KUBECTL_CONTEXT=bastion-man-man-ydb-dev
+JUMP_HOST_NAMESPACE=ydb-dev
+
 YDB_TOKEN=$(npc --profile newbius iam get-access-token)
-kubectl config use-context ik8s-beta
+kubectl config use-context $SLICE_KUBECTL_CONTEXT
 
 # Copy config to jumphost node
-kubectl -n ydb-global cp $1 ydb-jump-host-0:/tmp/dynconfig.yaml
+kubectl -n $JUMP_HOST_NAMESPACE cp $1 ydb-jump-host-0:/tmp/dynconfig.yaml
 
 # Call ydb admin config replace
-kubectl -n ydb-global exec -it ydb-jump-host-0 -- env YDB_TOKEN=$YDB_TOKEN ydb -e grpcs://man0-0505.ydb-dev.ik8s.nebiuscloud.net:2135 -d /ydb-slice-1 admin config replace -f /tmp/dynconfig.yaml
+kubectl -n $JUMP_HOST_NAMESPACE exec -it ydb-jump-host-0 -- env YDB_TOKEN=$YDB_TOKEN ydb -e grpcs://man0-0505.ydb-dev.ik8s.nebiuscloud.net:2135 -d /ydb-slice-1 admin config replace -f /tmp/dynconfig.yaml
