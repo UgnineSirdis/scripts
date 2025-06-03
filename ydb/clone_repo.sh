4aa92f6bc499b2beb284abfdf372babb0ef9795d
Vasily Gerasimov <UgnineSirdis@ydb.tech>
2025-06-03 14:28:48 +0200

    Clone repo script param

--- ydb/clone_repo.sh
+++ ydb/clone_repo.sh
@@ -1,9 +1,14 @@
 #! /usr/bin/env bash
 set -e
 
+DIR=$1
+if [ -z "$DIR" ] ; then
+    DIR=ydb
+fi
+
 cd $HOME
-git clone git@github.com:UgnineSirdis/ydb.git
-cd $HOME/ydb
+git clone git@github.com:UgnineSirdis/ydb.git $DIR
+cd $HOME/$DIR
 git branch -m main origin-main
 git remote add -m main upstream git@github.com:ydb-platform/ydb.git
 git fetch upstream main
