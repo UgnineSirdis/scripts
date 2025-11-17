8d78d0cbe6e91b0af9fbc2f766ad33dc7fee7820
Vasily Gerasimov <UgnineSirdis@ydb.tech>
2025-11-17 16:00:11 +0100

    Build in deploy_local_ydb alias

--- bashrc.sh
+++ bashrc.sh
@@ -54,7 +54,7 @@ alias tls='tmux ls'
 # local_ydb
 alias build_restart_local_ydb='~/ydb/ya make --build relwithdebinfo ~/ydb/ydb/apps/ydbd ~/ydb/ydb/public/tools/local_ydb ~/ydb/ydb/apps/ydb && pgrep -u $USER ydbd | xargs --no-run-if-empty kill && MON_PORT=28040 GRPC_PORT=17690 GRPC_TLS_PORT=17691 IC_PORT=17692 GRPC_EXT_PORT=17693 PUBLIC_HTTP_PORT=28041 ~/ydb/ydb/public/tools/local_ydb/local_ydb start --ydb-binary-path ~/ydb/ydb/apps/ydbd/ydbd --fixed-ports --ydb-working-dir'
 alias restart_local_ydb='pgrep -u $USER ydbd | xargs --no-run-if-empty kill && MON_PORT=28040 GRPC_PORT=17690 GRPC_TLS_PORT=17691 IC_PORT=17692 GRPC_EXT_PORT=17693 PUBLIC_HTTP_PORT=28041 ~/ydb/ydb/public/tools/local_ydb/local_ydb start --ydb-binary-path ~/ydb/ydb/apps/ydbd/ydbd --fixed-ports --ydb-working-dir'
-alias deploy_local_ydb='pgrep -u $USER ydbd | xargs --no-run-if-empty kill && MON_PORT=28040 GRPC_PORT=17690 GRPC_TLS_PORT=17691 IC_PORT=17692 GRPC_EXT_PORT=17693 PUBLIC_HTTP_PORT=28041 ~/ydb/ydb/public/tools/local_ydb/local_ydb deploy --ydb-binary-path ~/ydb/ydb/apps/ydbd/ydbd --fixed-ports --ydb-working-dir'
+alias deploy_local_ydb='~/ydb/ya make --build relwithdebinfo ~/ydb/ydb/apps/ydbd ~/ydb/ydb/public/tools/local_ydb ~/ydb/ydb/apps/ydb && pgrep -u $USER ydbd | xargs --no-run-if-empty kill && MON_PORT=28040 GRPC_PORT=17690 GRPC_TLS_PORT=17691 IC_PORT=17692 GRPC_EXT_PORT=17693 PUBLIC_HTTP_PORT=28041 ~/ydb/ydb/public/tools/local_ydb/local_ydb deploy --ydb-binary-path ~/ydb/ydb/apps/ydbd/ydbd --fixed-ports --ydb-working-dir'
 
 # vscode
 alias clear_vscode_proc='pgrep -f -u $USER .vscode-server | xargs --no-run-if-empty kill && pgrep -f -u $USER .ya/tools | xargs --no-run-if-empty kill'
