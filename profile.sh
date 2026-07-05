2c6474e51b4dc31ee5aa6c2c495456c40ac52300
Vasily Gerasimov <UgnineSirdis@ydb.tech>
2026-07-05 12:28:30 +0200

    claude env vars

--- profile.sh
+++ profile.sh
@@ -3,6 +3,10 @@
 PATH="$SCRIPTS_DIR:/usr/libexec/docker/cli-plugins:$HOME/bin:$HOME/bin/go/bin:$HOME/go/bin:$PATH"
 export TMPDIR="$HOME/tmp"
 
+# Claude proxy
+export ANTHROPIC_BASE_URL=https://nai-gw.nebius.ts.net/anthropic
+export ANTHROPIC_API_KEY=sk-ant-api03-gateway-placeholder
+
 cd ~/ydb
 
 if [ -z "$TMUX" ] ; then
